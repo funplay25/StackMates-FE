544a8afd6dfbb45cef3a10134852fe134daeef60
Pranav <bizzareforyou@gmail.com>
2026-03-21 21:29:25 +0530

    enabled credentials for token auth

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 import { BE_BASE_URL } from "./constants";
 
 const createSocketConnection = () => {
-  return io(BE_BASE_URL);
+  return io(BE_BASE_URL, { withCredentials: true });
 };
 
 export default createSocketConnection;
