8398731e0efa8f61740e80652c415cc521b4ed9c
Pranav <bizzareforyou@gmail.com>
2026-03-22 12:48:30 +0530

    applied working logic for production as well

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,7 +2,11 @@ import { io } from "socket.io-client";
 import { BE_BASE_URL } from "./constants";
 
 const createSocketConnection = () => {
-  return io(BE_BASE_URL, { withCredentials: true });
+  if (location.hostname === "localhost") {
+    return io(BE_BASE_URL, { withCredentials: true });
+  } else {
+    return io("/", { path: "/api/socket.io" });
+  }
 };
 
 export default createSocketConnection;
