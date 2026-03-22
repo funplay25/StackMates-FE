8dee3ea2f87b00827f620277f7c94c0067418f5c
Pranav <bizzareforyou@gmail.com>
2026-03-22 12:46:54 +0530

    page increment logic applied

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,35 +2,46 @@ import { useDispatch, useSelector } from "react-redux";
 import UserCard from "./UserCard";
 import axios from "axios";
 import { BE_BASE_URL } from "../utils/constants";
-import { addFeed, removeFeed } from "../utils/feedSlice";
+import { addFeed, incrementPage, removeFeed } from "../utils/feedSlice";
 import { useEffect } from "react";
 
 const Feed = () => {
   const dispatch = useDispatch();
-  const feed = useSelector((store) => store.feed);
+  const feed = useSelector((store) => store.feed.data);
+  const page = useSelector((store) => store.feed.page);
   const user = useSelector((store) => store.user);
 
-  const fetchFeed = async () => {
+  const fetchFeed = async (pageNum) => {
     try {
-      const res = await axios.get(BE_BASE_URL + "/feed", {
+      const res = await axios.get(`${BE_BASE_URL}/feed?${pageNum}&limit=10`, {
         withCredentials: true,
       });
 
       dispatch(addFeed(res.data));
     } catch (err) {
-      console.error("error while fetching feed" + err.message);
+      console.error("error while fetching feed " + err.message);
     }
   };
 
   useEffect(() => {
     if (!user?._id) return;
     dispatch(removeFeed());
-    fetchFeed();
+    fetchFeed(1);
   }, [user?._id]);
+
+  useEffect(() => {
+    if (!user?._id || page === 1) return;
+    fetchFeed(page);
+  }, [page]);
+
+  const handleLoadMore = () => {
+    if (feed.length === 0) return;
+    dispatch(incrementPage());
+  };
 
   return (
     <div>
-      <UserCard persons={feed} />
+      <UserCard persons={feed} onLoadMore={handleLoadMore} />
     </div>
   );
 };
